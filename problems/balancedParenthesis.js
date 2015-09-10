d93e2080fac49586020725a6801ac62322147fac
Yoshio Varney <yoshio.varney@gmail.com>
2015-09-09 22:56:13 -0700

    Finished balancedParens

--- problems/balancedParenthesis.js
+++ problems/balancedParenthesis.js
@@ -2,5 +2,28 @@
 // Extra credit: solve for [] & {}
 
 var isBalanced = function(string) {
+  var stack = [];
+  var balanced = {
+    "(": ")",
+    "[": "]",
+    "{": "}"
+  }
+  for (var i = 0; i < string.length; i++) {
+    var checkVal = string[i];
+    // check if key exists
+    if (balanced[checkVal]) {
+      stack.push(checkVal);
+      // skip to next iteration
+      continue;
+    }
+    
+    var lastItemInStack = stack[stack.length - 1];
+    // if checkVal is a value in balanced (only closing parens)
+    if (checkVal === balanced[lastItemInStack]) {
+      stack.pop();
+    }
+  }
 
+  // if stack still has items, parens not balanced
+  return stack.length === 0;
 };
