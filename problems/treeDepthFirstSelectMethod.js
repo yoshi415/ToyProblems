17e0e8be25b532de74196f5f2aaceadc45fa7e6f
Yoshio Varney <yoshio.varney@gmail.com>
2015-09-15 08:35:24 -0700

    Started treeDepthFirstSelectMethod.js

---
+++ problems/treeDepthFirstSelectMethod.js
@@ -0,0 +1,8 @@
+// Implement a depth-first method on a tree class.
+
+// DFSelect accepts a filter function, calls that function on each of the nodes in Depth First order, 
+// and returns a flat array of node values of the tree for which the filter returns true.
+
+Tree.prototype.DFSelect = function(filter) {      
+
+};
