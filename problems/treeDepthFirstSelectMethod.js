631cfa1d69cad5952bd8873ac11927b98ac3f1e7
Yoshio Varney <yoshio.varney@gmail.com>
2015-09-23 23:40:44 -0700

    finished treeDFS traversal

--- problems/treeDepthFirstSelectMethod.js
+++ problems/treeDepthFirstSelectMethod.js
@@ -4,5 +4,20 @@
 // and returns a flat array of node values of the tree for which the filter returns true.
 
 Tree.prototype.DFSelect = function(filter) {      
+  var results = [];
+  // depth is used as stack counter
+  var subroutine = function(node, depth) {
+    if (filter(node.value, depth)) {
+      results.push(node.value);
+    }
+    for (var i = 0; i < node.children.length; i++) {
+      var child = node.children[i];
+      // recurse on each child & increment depth to go down one level
+      subroutine(child, depth + 1);
+    }
+  };
+  // 'this' is the head node
+  subroutine(this, 0);
 
+  return results;
 };
